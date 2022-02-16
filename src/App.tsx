b8aacddefe3d369f883cfd24b84d55174c700e43
Chupriarti <wallx13@gmail.com>
2022-02-16 20:51:04 +0300

    Revert "add example with counter"
    This reverts commit 99620bb40f902d54130b9749d37bc178cd739e54.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css'
-import { useAppDisptach, useAppSelector } from './hooks/redux';
-import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
-  const {count} = useAppSelector(state => state.userReducer)
-  const {increment} = userSlice.actions
-  const dispatch = useAppDisptach()
-
   return (
     <div>
       Redux Toolkit Typescript
-      <h1>{count}</h1>
-      <button onClick={() => dispatch(increment(10))}>INCREMENT</button>
     </div>
   );
 }
