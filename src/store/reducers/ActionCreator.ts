c2a9cdf4ad26634e7c01d37866fcb101a13d4596
Chupriarti <wallx13@gmail.com>
2022-02-17 21:28:26 +0300

    use reducers

--- src/store/reducers/ActionCreator.ts
+++ src/store/reducers/ActionCreator.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 import { IUser } from "../../models/IUser";
 import { AppDispatch } from "../store";
+import { userSlice } from "./UserSlice"
 
 
 export const fetchUser = () => async (dispatch: AppDispatch) => {
     try {
+        dispatch(userSlice.actions.userFetching())
         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+        dispatch(userSlice.actions.userFetchingSuccess(response.data))
     } catch (e) {
-
+        dispatch(userSlice.actions.userFetchingError((e as any).message))
     }
 }
