3ca7b419754e0730e770c2dc951109a0706a7b79
Chupriarti <wallx13@gmail.com>
2022-02-17 21:33:35 +0300

    add getting users in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css'
+import { useAppDisptach, useAppSelector } from './hooks/redux';
+import { fetchUsers } from './store/reducers/ActionCreator';
 
 function App() {
+  const dispatch = useAppDisptach()
+  const {users} = useAppSelector(state => state.userReducer)
+
+  React.useEffect(() => {
+    dispatch(fetchUsers())
+  }, [])
+
   return (
     <div>
-      Redux Toolkit Typescript
+      {JSON.stringify(users, null, 2)}
     </div>
   );
 }
