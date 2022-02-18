70b8f910833f05eb0b960548c25bd14419580430
Chupriarti <wallx13@gmail.com>
2022-02-18 22:28:13 +0300

    fix naming

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-export const PostApi = createApi({
+export const postAPI = createApi({
     reducerPath: 'postAPI',
     baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
     endpoints: (build) => ({
