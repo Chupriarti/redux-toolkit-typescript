45906b73712e775fdb71bf195ff9363c882a6f76
Chupriarti <wallx13@gmail.com>
2022-02-18 23:02:42 +0300

    add Posts to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import './App.css'
+import PostContainer from './components/PostContainer';
 import { useAppDisptach, useAppSelector } from './hooks/redux';
 import { fetchUsers } from './store/reducers/ActionCreator';
 
 function App() {
-  const dispatch = useAppDisptach()
-  const { users, isLoading, error } = useAppSelector(state => state.userReducer)
+  // const dispatch = useAppDisptach()
+  // const { users, isLoading, error } = useAppSelector(state => state.userReducer)
 
-  React.useEffect(() => {
-    dispatch(fetchUsers())
-  }, [])
+  // React.useEffect(() => {
+  //   dispatch(fetchUsers())
+  // }, [])
 
   return (
     <div>
-      {isLoading && <h1>Loading...</h1>}
+      {/* {isLoading && <h1>Loading...</h1>}
       {error && <h1>{error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      {JSON.stringify(users, null, 2)} */}
+      <PostContainer />
     </div>
   );
 }
