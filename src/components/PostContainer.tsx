755ad5a228a5dbd27d6be557b79d725a9d083ea4
Chupriarti <wallx13@gmail.com>
2022-02-18 23:09:15 +0300

    add loading and error indications

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -3,9 +3,11 @@ import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
 
 const PostContainer = () => {
-    const {data: posts} = postAPI.useFetchAllPostsQuery(5)
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5)
     return (
         <div className="post__list">
+            {isLoading && <h1>Loading...</h1>}
+            {error && <h1>Error occurred during loading</h1>}
             {posts && posts.map(post => 
                 <PostItem key={post.id} post={post} />
             )}
