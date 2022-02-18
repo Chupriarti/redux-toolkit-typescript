5e48cd66076e72f1d003acb3b769644ba7f71166
Chupriarti <wallx13@gmail.com>
2022-02-18 22:26:25 +0300

    add PostService

---
+++ src/services/PostService.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const PostApi = createApi({
+    reducerPath: 'postAPI',
+    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
+    endpoints: (build) => ({
+        fetchAllPosts: build.query({
+            query: () => ({
+                url: '/posts'
+            })
+        })
+    })
+})
