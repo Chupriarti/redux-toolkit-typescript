3ce8090ffaff74b78398503cb42b35e1757bee94
Chupriarti <wallx13@gmail.com>
2022-02-19 09:53:42 +0300

    add new post mutation

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { IPost } from '../models/IPost'
 import { postAPI } from '../services/PostService'
 import PostItem from './PostItem'
 
 const PostContainer = () => {
-    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5)
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(100)
+    const [createPost, {}] = postAPI.useCreatePostMutation()
+    
+    const handleCreate =  async () => {
+        const title = prompt()
+        await createPost({title, body: title} as IPost)
+    }
+    
     return (
         <div className="post__list">
+            <button onClick={handleCreate}>Add new post</button>
             {isLoading && <h1>Loading...</h1>}
             {error && <h1>Error occurred during loading</h1>}
             {posts && posts.map(post => 
